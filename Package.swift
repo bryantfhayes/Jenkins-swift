4736869f55b8914d1022773faa022433259d8c55
Bryant Hayes <bryantfhayes@gmail.com>
2023-01-31 17:04:28 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "Jenkins"
+    name: "Jenkins",
+    platforms: [
+        .macOS(.v10_14)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Jenkins",
+            targets: ["Jenkins"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Jenkins",
+            exclude: [],
+            resources: []
+        )
+        .testTarget(
+            name: "JenkinsTests",
+            dependencies: ["Jenkins"]),
+    ]
 )
